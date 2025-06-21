9dc3e137619ccb5f7dc14d79433cdd85688e6408
Udit <145645636+uditttttt@users.noreply.github.com>
2025-06-21 18:43:16 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,16 @@
+// require('dotenv').config()
+// const app = require('./src/app')
+
+// app.listen(3000 , () => {
+//   console.log('Server is running on http://localhost:3000');
+// })
+
+
 require('dotenv').config()
 const app = require('./src/app')
 
-app.listen(3000 , () => {
-  console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 })
